57af222aa3c1b8a2b9c1621df0394d0060db8eee
tbricca <tbricca@gmail.com>
2017-09-01 08:46:34 -0700

    clock

---
+++ clock.js
@@ -0,0 +1,34 @@
+var secondHand = document.getElementById('second');
+var minuteHand = document.getElementById('minute');
+var hourHand = document.getElementById ('second');
+
+function secHand(){
+    var counter = 0;
+    setInterval(function(){
+        counter++;
+        secondHand.style.transform = "rotate(" + counter + "deg)";
+    }, 1000);
+}
+
+function minHand(){
+	var counter = 0;
+	setInterval(function(){
+		counter=+60;
+		minHand.style.transform = "rotate(" + counter + "deg)";
+	}, 60000);
+}
+
+function hrHand(){
+	var counter = 0;
+	setInterval(function(){
+		counter=+30;
+		hourHand.style.transform = "rotate(" + counter + "deg)";
+	}, 360000000);
+}
+
+secHand();
+minHand(); 
+hrHand();
+
+
+
